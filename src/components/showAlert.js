18dc50f99f7018e9af755cd6d1524f8d503a4403
jon-cunicella <joncunicella@gmail.com>
2019-07-30 14:52:00 -0400

    daniel is a wizard

---
+++ src/components/showAlert.js
@@ -0,0 +1,3 @@
+export default function showAlert() {
+  window.alert("Welcome to Magic Date Ball. We're here to bust some balls.");
+}
--- src/config/config.js
+++
@@ -1,4 +0,0 @@
-// # set environment variables
-
-export default GOOGLE_PLACES_API_KEY = ""
-export default GOOGLE_PLACES_OUTPUT_FORMAT = "json"
