cd5d3b84a216311f0c55a57361dd1ebe30d48952
jon-cunicella <joncunicella@gmail.com>
2019-07-30 15:17:36 -0400

    create eight ball

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import showAlert from './components/showAlert';
+import '../public/stylesheets/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      <header className="App-header">
+        <h1 className="DaRealHeadah">Magic Date Ball</h1>
+      </header>
+      <div className="ball-outer">
+        <div className="ball-inner" />
+      </div>
 
       <button onClick={showAlert}>Shake It UP</button>
     </div>
