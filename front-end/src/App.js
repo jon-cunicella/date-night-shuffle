dd476cc45dbd17ebf4de86f2b8ed66bed72dbdc7
clayton-scherer <cscherer93@aol.com>
2019-08-09 11:46:58 -0400

    layout restructured and styling work on ui

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import Layout from './javascript/utils/components/Layout';
-import PlaceCardList from './javascript/utils/components/PlaceCardList';
-import FormComponent from './javascript/utils/components/FormComponent';
+import React, { useEffect, useState } from "react";
+import Layout from "./javascript/utils/components/Layout";
+import PlaceCardList from "./javascript/utils/components/PlaceCardList";
 
 const App = () => {
   const [areas, setAreas] = useState([]);
-  const [title, setTitle] = useState('');
   const [area, setArea] = useState({ places: [] });
 
-  
   // make method to sort by price that changes state of area.places. That way we dont need to hit db
   useEffect(() => {
-    fetch('http://localhost:4000/api/areas')
+    fetch("http://localhost:4000/api/areas")
       .then(res => res.json())
       .then(areas => setAreas(areas));
-
-    setTitle('Date Night Roulette');
   }, []);
+
   const getArea = userAreaId => {
     fetch(`http://localhost:4000/api/areas/${userAreaId}`)
       .then(res => res.json())
@@ -25,8 +21,7 @@ const App = () => {
 
   return (
     <div className="wrapper">
-      <Layout title={title}>
-        <FormComponent areas={areas} getArea={getArea} />
+      <Layout areas={areas} getArea={getArea}>
         <PlaceCardList places={area.places} />
       </Layout>
     </div>
