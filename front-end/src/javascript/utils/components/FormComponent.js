68d73f0603066608a04eb911ce3a36ec11f4bfcb
clayton-scherer <cscherer93@aol.com>
2019-08-09 14:58:20 -0400

    conditional rendering on the form components

--- front-end/src/javascript/utils/components/FormComponent.js
+++ front-end/src/javascript/utils/components/FormComponent.js
@@ -7,53 +7,80 @@ class FormComponent extends React.Component {
       areas: { areas },
       area: {},
       userArea: "",
-      userPrice: ""
+      userPrice: "",
+      isSelected: false
     };
   }
 
   handleAreaChange = e => {
     const userAreaId = e.target.value;
     this.props.getArea(userAreaId);
+    this.setState({ isSelected: true });
   };
 
-  render() {
-    const areas = this.props.areas;
-    return (
-      <form className="wheel-form">
-        <select
-          name="areas"
-          id="area-dropdown"
-          onChange={this.handleAreaChange}
-        >
-          <option value="select-area" disabled selected>
-            Select an Area
-          </option>
-          {areas.map(area => (
-            <option className="areaToSelect" key={area._id} value={area._id}>
-              {area.name}
+  handleFormComponents = ({ isSelected }) => {
+    if (isSelected) {
+      const areas = this.props.areas;
+      return (
+        <form className="wheel-form">
+          <select
+            name="areas"
+            id="area-dropdown"
+            onChange={this.handleAreaChange}
+          >
+            <option value="select-area" disabled selected>
+              Select an Area
             </option>
-          ))}
-        </select>
-        <select
-          name="prices"
-          id="price-dropdown"
-          onChange={this.handlePriceChange}
-        >
-          <option value="select-price" disabled selected>
-            Select a Price
-          </option>
-          <option className="price" value={this.state.userPrice}>
-            $
-          </option>
-          <option className="price" value={this.state.userPrice}>
-            $$
-          </option>
-          <option className="price" value={this.state.userPrice}>
-            $$$
-          </option>
-        </select>
-      </form>
-    );
+            {areas.map(area => (
+              <option className="areaToSelect" key={area._id} value={area._id}>
+                {area.name}
+              </option>
+            ))}
+          </select>
+          <select
+            name="prices"
+            id="price-dropdown"
+            onChange={this.handlePriceChange}
+          >
+            <option value="select-price" disabled selected>
+              Select a Price
+            </option>
+            <option className="price" value={this.state.userPrice}>
+              $
+            </option>
+            <option className="price" value={this.state.userPrice}>
+              $$
+            </option>
+            <option className="price" value={this.state.userPrice}>
+              $$$
+            </option>
+          </select>
+        </form>
+      );
+    } else {
+      const areas = this.props.areas;
+      return (
+        <form className="wheel-form">
+          <select
+            name="areas"
+            id="area-dropdown"
+            onChange={this.handleAreaChange}
+          >
+            <option value="select-area" disabled selected>
+              Select an Area
+            </option>
+            {areas.map(area => (
+              <option className="areaToSelect" key={area._id} value={area._id}>
+                {area.name}
+              </option>
+            ))}
+          </select>
+        </form>
+      );
+    }
+  };
+  render() {
+    return <this.handleFormComponents isSelected={this.state.isSelected} />;
   }
 }
 export default FormComponent;
