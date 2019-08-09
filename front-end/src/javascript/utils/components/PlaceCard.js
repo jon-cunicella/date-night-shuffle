f4f3cc527cc544e5b636a7fbb10e412f92ea1297
clayton-scherer <cscherer93@aol.com>
2019-08-09 13:53:24 -0400

    layout updated to resemble playing card

--- front-end/src/javascript/utils/components/PlaceCard.js
+++ front-end/src/javascript/utils/components/PlaceCard.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const PlaceCard = ({ place }) => {
   return (
-    <article className="place-card-info">
+    <>
+      <div className="priceRating--inverted">
+        <h4 className="place-card__price--inverted"> {place.price}</h4>
+        <small className="place-card__rating--inverted"> {place.rating}</small>
+      </div>
       <h3 className="place-card__title">{place.name}</h3>
-      <h4 className="place-card__price">Price: {place.price}</h4>
-      <small className="place-card__rating">Rating: {place.rating}</small>
-    </article>
+      <div className="priceRating">
+        <h4 className="place-card__price"> {place.price}</h4>
+        <small className="place-card__rating"> {place.rating}</small>
+      </div>
+    </>
   );
 };
 
