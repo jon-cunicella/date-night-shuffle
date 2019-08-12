e9ccf2fcb725f85c546ad6e9f9b9cf6480cc5517
jon-cunicella <joncunicella@gmail.com>
2019-08-12 15:04:43 -0400

    Turned placecard into button

--- front-end/src/javascript/utils/components/PlaceCard.js
+++ front-end/src/javascript/utils/components/PlaceCard.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React from 'react';
 
 const PlaceCard = ({ place }) => {
+  const handlePopup = e => {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  };
   return (
     <>
-      <div className="priceRating--inverted">
-        <h4 className="place-card__price--inverted"> {place.price}</h4>
-        <small className="place-card__rating--inverted"> {place.rating}</small>
-      </div>
-      <h3 className="place-card__title">{place.name}</h3>
-      <div className="priceRating">
-        <h4 className="place-card__price"> {place.price}</h4>
-        <small className="place-card__rating"> {place.rating}</small>
-      </div>
+      <a href="test" onClick="{handlePopup}">
+        <div className="priceRating--inverted">
+          <h4 className="place-card__price--inverted"> {place.price}</h4>
+          <small className="place-card__rating--inverted">
+            {' '}
+            {place.rating}
+          </small>
+        </div>
+        <h3 className="place-card__title">{place.name}</h3>
+        <div className="priceRating">
+          <h4 className="place-card__price"> {place.price}</h4>
+          <small className="place-card__rating"> {place.rating}</small>
+        </div>
+      </a>
     </>
   );
 };
