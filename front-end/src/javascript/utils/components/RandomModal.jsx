b3b77b00b882f0f99cac9bf035c8805b059882a5
jon-cunicella <joncunicella@gmail.com>
2019-08-15 11:33:01 -0400

    Modal looks better

--- front-end/src/javascript/utils/components/RandomModal.jsx
+++ front-end/src/javascript/utils/components/RandomModal.jsx
@@ -7,11 +7,20 @@ const RandomModal = ({ closeRandomModal, showRandomModal, place }) => {
 
   return (
     <div className={showHideClassName}>
-      <section className='modal-main'>
-        <h1 className='modal-place__title'>{place.name}</h1>
-        <h4 className='modal-place__price'> {place.price}</h4>
-        <small className='modal-place__rating'> {place.rating}</small>
-        <button onClick={closeRandomModal}>&times;</button>
+      <section className="modal-main">
+        <button className="modal-button" onClick={closeRandomModal}>
+          &times;
+        </button>
+        <div className="modal-content">
+          <div className="modal-content__container">
+            <h1 className="modal-place__title">{place.name}</h1>
+            <h4 className="modal-place__price"> Price: {place.price}</h4>
+            <image className="modal-place__image" />
+            <small className="modal-place__rating">
+              Rating: {place.rating}
+            </small>
+          </div>
+        </div>
       </section>
     </div>
   );
