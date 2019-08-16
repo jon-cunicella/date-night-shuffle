f2217a89285dd83ece855c6ab0bf5a21defa2732
Chris-Ross <ChrisJRossJr@gmail.com>
2019-08-16 10:46:07 -0400

    partially working map. Need to add info window correctly

--- front-end/src/javascript/utils/components/GoogleMap.jsx
+++ front-end/src/javascript/utils/components/GoogleMap.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
@@ -12,9 +12,50 @@ export class GoogleMap extends Component {
     this.state = {
       name: this.props.name,
       lat: this.props.lat,
-      long: this.props.long
+      long: this.props.long,
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
     };
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
   }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+    this.displayPaper();
+  };
+  onMapClick = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  displayMarkers = () => {
+    return (
+      <Marker
+        position={{
+          lat: this.state.long,
+          lng: this.state.lat,
+          title: this.state.name
+        }}
+        onClick={() => {
+          this.onMarkerClick();
+        }}
+      />
+    );
+  };
+
+  displayPaper = () => {
+    return <p>{this.state.name}</p>;
+  };
 
   render() {
     return (
@@ -26,7 +67,16 @@ export class GoogleMap extends Component {
           lat: this.state.long,
           lng: this.state.lat
         }}
-      />
+      >
+        {this.displayMarkers()}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClick={() => {
+            this.onMapClick();
+          }}
+        />
+      </Map>
     );
   }
 }
