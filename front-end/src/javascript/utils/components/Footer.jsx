81212d2da66eb3a9de6555e99126c0901d035af9
clayton-scherer <cscherer93@aol.com>
2019-08-19 15:16:49 -0400

    changed footer and title, styling changes

--- front-end/src/javascript/utils/components/Footer.jsx
+++ front-end/src/javascript/utils/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 export default function RenderFooter() {
+  const date = new Date();
+
   return (
     <footer className="footer">
       {/* dynamic date for the copyright */}
-      <small className="copy">&copy; 2019 - Date Night Roulette</small>
+      <small className="copy">
+        &copy; {date.getFullYear} - Date Night Shuffle
+      </small>
     </footer>
   );
 }
